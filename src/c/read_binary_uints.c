ba866d7eed129e63bb06b585ad13bd070e52693d
Ryan Layer <ryan.layer@gmail.com>
2015-01-31 13:46:39 -0500

    show 64 or 32

--- src/c/read_binary_uints.c
+++ src/c/read_binary_uints.c
@@ -7,35 +7,33 @@
 int main(int argc, char **argv)
 {
 
-    if (argc != 2){
+    if (argc != 3){
         fprintf(stderr,
-                "usage:\t%s <input file>\n",
+                "usage:\t%s <size> <input file>\n",
                 argv[0]);
         return 1;
     }
 
-    char *file_name = argv[1];
+    int size = atoi(argv[1]);
+    char *file_name = argv[2];
     FILE *ptr_myfile;
-    uint64_t my_record;
-    //uint32_t my_record;
-
     ptr_myfile=fopen(file_name,"rb");
     if (!ptr_myfile) {
         printf("Unable to open file!");
         return 1;
     }
 
-    while ( fread(&my_record,sizeof(uint64_t),1,ptr_myfile) == 1) {
-        printf("%" PRIu64 "\n",my_record);
+    if (size == 32) {
+        uint32_t my_record;
+        while ( fread(&my_record,sizeof(uint32_t),1,ptr_myfile) == 1) {
+            printf("%" PRIu32 "\n",my_record);
+        }
+    } else if (size == 64) {
+        uint64_t my_record;
+        while ( fread(&my_record,sizeof(uint64_t),1,ptr_myfile) == 1) {
+            printf("%" PRIu64 "\n",my_record);
+        }
     }
-
-    /*
-    while ( fread(&my_record,sizeof(uint32_t),1,ptr_myfile) == 1) {
-        printf("%" PRIu32 "\n",my_record);
-    }
-    */
-
-
     fclose(ptr_myfile);
     return 0;
 }
